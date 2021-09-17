499ad81e47436a6d594f8fea8b5f4181282c76bf
Vasyl Hryts <vasylhryts74@gmail.com>
2021-09-17 18:45:36 +0300

    commit 2

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,7 +3,10 @@ import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
 import data from '../data.js';
 import User from '../models/userModel.js';
-import { generateToken, isAuth } from '../utils.js';
+import {
+  generateToken,
+  isAuth
+} from '../utils.js';
 
 const userRouter = express.Router();
 
@@ -12,14 +15,18 @@ userRouter.get(
   expressAsyncHandler(async (req, res) => {
     // await User.remove({});
     const createdUsers = await User.insertMany(data.users);
-    res.send({ createdUsers });
+    res.send({
+      createdUsers
+    });
   })
 );
 
 userRouter.post(
   '/signin',
   expressAsyncHandler(async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({
+      email: req.body.email
+    });
     if (user) {
       if (bcrypt.compareSync(req.body.password, user.password)) {
         res.send({
@@ -32,7 +39,9 @@ userRouter.post(
         return;
       }
     }
-    res.status(401).send({ message: 'Invalid email or password' });
+    res.status(401).send({
+      message: 'Invalid email or password'
+    });
   })
 );
 
@@ -61,7 +70,9 @@ userRouter.get(
     if (user) {
       res.send(user);
     } else {
-      res.status(404).send({ message: 'User Not Found' });
+      res.status(404).send({
+        message: 'User Not Found'
+      });
     }
   })
 );
