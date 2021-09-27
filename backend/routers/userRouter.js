87110a0f4be5f20140a790c633228b6deaa0b57e
Vasyl Hryts <vasylhryts74@gmail.com>
2021-09-27 23:22:52 +0300

    before bootstrap

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,11 +3,7 @@ import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
 import data from '../data.js';
 import User from '../models/userModel.js';
-import {
-  generateToken,
-  isAuth
-} from '../utils.js';
-
+import { generateToken, isAdmin, isAuth } from '../utils.js';
 const userRouter = express.Router();
 
 userRouter.get(
@@ -15,18 +11,14 @@ userRouter.get(
   expressAsyncHandler(async (req, res) => {
     // await User.remove({});
     const createdUsers = await User.insertMany(data.users);
-    res.send({
-      createdUsers
-    });
+    res.send({ createdUsers });
   })
 );
 
 userRouter.post(
   '/signin',
   expressAsyncHandler(async (req, res) => {
-    const user = await User.findOne({
-      email: req.body.email
-    });
+    const user = await User.findOne({ email: req.body.email });
     if (user) {
       if (bcrypt.compareSync(req.body.password, user.password)) {
         res.send({
@@ -39,9 +31,7 @@ userRouter.post(
         return;
       }
     }
-    res.status(401).send({
-      message: 'Invalid email or password'
-    });
+    res.status(401).send({ message: 'Invalid email or password' });
   })
 );
 
@@ -70,9 +60,7 @@ userRouter.get(
     if (user) {
       res.send(user);
     } else {
-      res.status(404).send({
-        message: 'User Not Found'
-      });
+      res.status(404).send({ message: 'User Not Found' });
     }
   })
 );
@@ -95,6 +83,51 @@ userRouter.put(
         isAdmin: updatedUser.isAdmin,
         token: generateToken(updatedUser),
       });
+    }
+  })
+);
+userRouter.get(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.send(users);
+  })
+);
+userRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      if (user.email === 'admin@example.com') {
+        res.status(400).send({ message: 'Can Not Delete Admin User' });
+        return;
+      }
+      const deleteUser = await user.remove();
+      res.send({ message: 'User Deleted', user: deleteUser });
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
+userRouter.put(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.isSeller = req.body.isSeller || user.isSeller;
+      user.isAdmin = req.body.isAdmin || user.isAdmin;
+      const updatedUser = await user.save();
+      res.send({ message: 'User Updated', user: updatedUser });
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
     }
   })
 );
--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // eslint-disable-next-line no-undef
-mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost/FootballShop', {
+mongoose.connect(process.env.MONGODB_URL ||'mongodb+srv://vasyl:1q2w3e4r-@cluster0.jtyj1.mongodb.net/FootballShop?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 
@@ -26,15 +26,20 @@ app.get('/api/config/paypal', (req, res) => {
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.get('/',(req,res)=>{
-    res.send('Server is ready');
-});
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
 // eslint-disable-next-line no-undef
 const port=process.env.PORT || 5000;
-app.listen(5000, ()=>{
+app.listen(port, ()=>{
     console.log(`Server at http://localhost:${port}`);    
 });
